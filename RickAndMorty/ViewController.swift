6d785b3bcb021671bd42a69856dadda5f2a2d502
Aleksander Verestnikov <a.verestnikov@corp.mail.ru>
2021-03-30 11:55:40 +0300

    Initial Commit

---
+++ RickAndMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by a.verestnikov on 30.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
